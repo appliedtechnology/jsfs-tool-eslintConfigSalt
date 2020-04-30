bd255e792e876e88de0f807f65510a85b06f14d2
Levy Fekete <levy.fekete@gmail.com>
2020-04-30 09:34:07 +0200

    Add dependencies and main linting rules

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-module.exports = {};
+module.exports = {
+  extends: 'eslint-config-airbnb/base',
+  env: {
+    browser: true,
+    node: true,
+    mocha: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'arrow-parens': ['error', 'as-needed'],
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+  },
+};
