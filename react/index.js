24d0cc6033805c2689484f10e7f851e2d0970600
Levy Fekete <levy.fekete@gmail.com>
2020-08-18 16:47:24 +0200

    Fix linter issues

--- react/index.js
+++ react/index.js
@@ -1,13 +1,13 @@
 module.exports = {
   extends: [
-    "eslint-config-airbnb/rules/react",
-    "eslint-config-airbnb/rules/react-a11y",
+    'eslint-config-airbnb/rules/react',
+    'eslint-config-airbnb/rules/react-a11y',
   ].map(require.resolve),
   rules: {
-    "react/jsx-closing-bracket-location": [
-      "error",
-      { selfClosing: "after-props", nonEmpty: "after-props" },
+    'react/jsx-closing-bracket-location': [
+      'error',
+      { selfClosing: 'after-props', nonEmpty: 'after-props' },
     ],
-    "react/jsx-filename-extension": ["error", { extensions: [".js"] }],
+    'react/jsx-filename-extension': ['error', { extensions: ['.js'] }],
   },
 };
